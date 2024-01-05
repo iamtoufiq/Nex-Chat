fcc94bcbe2c86cb50b813a1cca3cfd75b8c48af4
Toufiq Choudhari <toufiq.c@finzmoney.com>
2024-01-05 12:30:36 +0530

    completed the conversation ui and functionality

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -2,19 +2,21 @@
 // import Sidebar from "../components/sidebar/Sidebar";
 // import UserList from "./components/UserList";
 
+import getUsers from "../actions/getUsers";
 import Sidebar from "../components/sidebar/Sidebar";
+import UserList from "./components/UserList";
 
 export default async function UsersLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  // const users = await getUsers();
-
+  const users = await getUsers();
+  // console.log("users", users);
   return (
     <Sidebar>
       <div className="h-full">
-        {/* <UserList items={users} /> */}
+        <UserList items={users} />
         {children}
       </div>
     </Sidebar>
